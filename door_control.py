594860565a174ceb63de4a494e8cabcaaf00a22d
Glenn-Herdrich <36314060+Glenn-Herdrich@users.noreply.github.com>
2022-06-29 00:07:51 -0700

    Update door_control.py

--- door_control.py
+++ door_control.py
@@ -1,91 +1,36 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 import pinout
+from configparser import ConfigParser
 
+config = ConfigParser()
 STEP = pinout.STEP
 DIR = pinout.DIR
 EN = pinout.EN
 POWER = pinout.POWER
-TRIGGER = pinout.TRIGGER
-ECHO = pinout.ECHO
 TOP_LIMIT = pinout.TOP_LIMIT
 BOT_LIMIT = pinout.BOT_LIMIT
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(EN, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(TRIGGER, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+GPIO.setup(EN, GPIO.OUT)
+GPIO.setup(POWER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(TOP_LIMIT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BOT_LIMIT, GPIO.IN, pull_up_down=GPIO.PUD_UP)   
-GPIO.setup(POWER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def door_status():
-    top_button = GPIO.input(TOP_LIMIT)
-    bot_button = GPIO.input(BOT_LIMIT)    
-    if top_button == 0 or bot_button == 0:
-        #Door is Open or Close
-        return True
-    if bot_button == 1 and top_button == 1:
-        #Door is in motion or broken :(
-        return False
-
-def path_status():
-    GPIO.output(TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIGGER, False)
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    while GPIO.input(ECHO) == 0:
-        StartTime = time.time()
-        
-    while GPIO.input(ECHO) == 1:
-        StopTime = time.time()
- 
-    TimeElapsed = StopTime - StartTime
-    distance = (TimeElapsed * 34300) / 2
-    return distance    
-    
-def motor_status():
-    
-    motor_en = GPIO.input(EN)
-    if motor_en == 0:      
-        GPIO.output(EN,1)
-        print("Motor enabled.")
-    
-    if motor_en == 1:
-        GPIO.output(EN,0)
-        print("Motor disabled.")
-
-def action_door(action):
+  
+def action_door(direction):
     GPIO.output(EN,1)
-
-    if action == 1:
-        print("Opening Door")
-        GPIO.output(DIR,1)
-    if action == 0:
-        print("Closing Door")
-        GPIO.output(DIR,0)
-      
+    GPIO.output(DIR,direction)
+    config.read('status.ini')
+    message = "Opening Door" if direction else "Closing Door"
+    print(message)
+    
     while True:
-        top_button = GPIO.input(TOP_LIMIT)
-        bot_button = GPIO.input(BOT_LIMIT)
-        power_buttom = GPIO.input(POWER)
-
-        if action == 1 and top_button == 0:
-            print("Door Open")
-            break
-        if action == 0 and bot_button == 0:
-            print("Door Close")
-            break
-        if power_buttom == 0:
-            GPIO.output(EN,0)
-            break
-        #if path_status() < 70:
-            #GPIO.output(DIR,1)
-        GPIO.output(STEP,GPIO.HIGH)
-        sleep(.001) 
-        GPIO.output(STEP,GPIO.LOW)
-        sleep(.001) 
-#GPIO.cleanup()
+        if (direction and not GPIO.input(TOP_LIMIT)) or (not direction and not GPIO.input(BOT_LIMIT)) or (not GPIO.input(POWER)) or (config.getboolean('door', 'path_status')) or (config.getboolean('door', 'path_status')): break          
+        #Additional option for Power and path status failures might be needed -- door open
+        else:
+            GPIO.output(STEP,GPIO.HIGH)
+            time.sleep(.001) 
+            GPIO.output(STEP,GPIO.LOW)
+            time.sleep(.001) 
