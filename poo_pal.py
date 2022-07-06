05ee8b2f5088559db0bc36f6584459cabdd97f8a
Glenn-Herdrich <36314060+Glenn-Herdrich@users.noreply.github.com>
2022-07-05 23:22:59 -0700

    Update poo_pal.py

--- poo_pal.py
+++ poo_pal.py
@@ -9,11 +9,11 @@ while True:
     for process in psutil.process_iter():
         if process.cmdline() == ['python', '/home/unknown/Documents/poo_pal/door_management.py']:
             management_active = 1
-    #     if process.cmdline() == ['python', '/home/unknown/Documents/poo_pal/door_scheduler.py']:
-    #        scheduler_active = 1       
+        if process.cmdline() == ['python', '/home/unknown/Documents/poo_pal/door_scheduler.py']:
+            scheduler_active = 1       
 
     if management_active == 0:
         Popen(['python', '/home/unknown/Documents/poo_pal/door_management.py'])
     
-    #if scheduler_active == 0:
-    #    Popen(['python', '/home/unknown/Documents/poo_pal/door_scheduler.py'])        
+    if scheduler_active == 0:
+        Popen(['python', '/home/unknown/Documents/poo_pal/door_scheduler.py'])        
